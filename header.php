c1040de3fe1267ad98768a53f6fc0d4db2627105
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2021-10-11 23:43:39 +0100

    New header js file

--- header.php
+++ header.php
@@ -24,71 +24,75 @@
           <a href="index.php">
             <img src="images/logo.png" alt="Home" />
           </a>
+          <div class="button">
+            <i class="fas fa-bars" id="button">
+              <span class="jsvar" id="jsvar"></span>
+            </i>
+          </div>
         </div>
-        <nav id="links">
-          <ul>
-            <li class="soon"><a href="#">About</a></li>
-            <li><a href="post.php">Post</a></li>
-            <li class="soon"><a href="#">Feed</a></li>
-            <li class="menu-handle">
-              <a href="#">Categories</a>
-              <ul class="menu">
-                <?php 
-                  require('categories.php');
-                  $number_of_categories = number_of_categories();
-                  $m = 1;
-                  while($m <= $number_of_categories) {
-                    $category = get_category_infos($m);
+        <div class="nav-container hidden" id="nav-container">
+          <nav>
+            <ul>
+              <li class="soon"><a href="#">About</a></li>
+              <li><a href="post.php">Post</a></li>
+              <li class="soon"><a href="#">Feed</a></li>
+              <li class="menu-handle">
+                <a href="#">Categories</a>
+                <ul class="menu">
+                  <?php 
+                    require('categories.php');
+                    $number_of_categories = number_of_categories();
+                    $m = 1;
+                    while($m <= $number_of_categories) {
+                      $category = get_category_infos($m);
+                      echo '
+                        <li><a href="config.php?option=filter_category&category='.$category['id'].'">'.$category['name'].'</a></li>
+                      ';
+                      $m = $m + 1;
+                    }
+                  ?>
+                </ul>
+              </li>
+              <li class="soon"><a href="#">Services</a></li>
+              <li><a href="mailto:abdelkarima46@gmail.com" target="_blank">Contact</a></li>
+            </ul>
+          </nav>
+          <nav>
+            <ul>
+              <?php 
+                  if(! isset($_SESSION['first_name'])) {
                     echo '
-                      <li><a href="config.php?option=filter_category&category='.$category['id'].'">'.$category['name'].'</a></li>
+                      <li>
+                        <a href="log.php">Log in</a>
+                      </li>
+                      <li>
+                        <a href="sign.php">sign up for free</a>
+                      </li>
                     ';
-                    $m = $m + 1;
                   }
-                ?>
-              </ul>
-            </li>
-            <li class="soon"><a href="#">Services</a></li>
-            <li><a href="mailto:abdelkarima46@gmail.com" target="_blank">Contact</a></li>
-          </ul>
-        </nav>
-        <nav id="sign">
-          <ul>
-            <?php 
-                if(! isset($_SESSION['first_name'])) {
-                  echo '
-                    <li>
-                      <a href="log.php">Log in</a>
-                    </li>
-                    <li>
-                      <a href="sign.php">sign up for free</a>
-                    </li>
-                  ';
-                }
-                else {
-                  echo '
-                    <li class="menu-handle">
-                      <a href="account.php">Abdelkarim</a>
-                      <ul class="menu">
-                        <li>
-                          <a href="account.php"><i class="fas fa-user"></i>Account</a>
-                        </li>
-                        <li>
-                          <a href="settings.php"><i class="fas fa-cog"></i>Settings</a>
-                        </li>
-                        <li>
-                          <a href="config.php?option=logout"
-                            ><i class="fas fa-sign-out-alt"></i>Log out</a
-                          >
-                        </li>
-                      </ul>
-                    </li>
-                  ';
-                }
-            ?>
-          </ul>
-        </nav>
-        <div class="button">
-          <i class="fas fa-arrow-down" id="button"><span class='jsvar' id='jsvar'></span></i>
+                  else {
+                    echo '
+                      <li class="menu-handle">
+                        <a href="account.php">Abdelkarim</a>
+                        <ul class="menu">
+                          <li>
+                            <a href="account.php"><i class="fas fa-user"></i>Account</a>
+                          </li>
+                          <li>
+                            <a href="settings.php"><i class="fas fa-cog"></i>Settings</a>
+                          </li>
+                          <li>
+                            <a href="config.php?option=logout"
+                              ><i class="fas fa-sign-out-alt"></i>Log out</a
+                            >
+                          </li>
+                        </ul>
+                      </li>
+                    ';
+                  }
+              ?>
+            </ul>
+          </nav>
         </div>
       </div>
     </header>
--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,11 @@
 function expand() {
-  document.getElementById("nav").className = "nav-menu";
-  document.getElementById("sign").className = "sign-menu";
-  document.getElementById("button").className = "fas fa-arrow-up";
+  document.getElementById("nav-container").className = "nav-container";
   var jsvar = document.getElementById("jsvar");
   var text = document.createTextNode('expanded');
   jsvar.appendChild(text);
 }
 function minify() {
-  document.getElementById("nav").className= "";
-  document.getElementById("sign").className = "sign";
-  document.getElementById("button").className = "fas fa-arrow-down";
+  document.getElementById("nav-container").className = "nav-container hidden";
   var jsvar = document.getElementById("jsvar");
   jsvar.removeChild(jsvar.childNodes[0]);
 }
